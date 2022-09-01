9f87fe9c3336fefcbc10561da33679c4e939b99b
jacksonLRD <jacksonlrdsantos@gmail.com>
2022-08-31 22:46:12 -0300

    feat: alterado nome da funcao de encerrar o servidor

--- src/app/config/server/server.ts
+++ src/app/config/server/server.ts
@@ -21,7 +21,7 @@ server.on('connection', (connection) => {
   connection.on('close', () => connections.filter((current) => current !== connection));
 });
 
-function gracefulShutDown(event: string) {
+function shutDown(event: string) {
   return (code: number | string) => {
     console.info(`Received kill signal, shutting down gracefully.\nEvent: ${event}\nCode: ${code}`);
     server.close(() => {
@@ -35,9 +35,9 @@ function gracefulShutDown(event: string) {
   };
 }
 
-process.on('SIGINT', gracefulShutDown('SIGINT'));
+process.on('SIGINT', shutDown('SIGINT'));
 
-process.on('SIGTERM', gracefulShutDown('SIGTERM'));
+process.on('SIGTERM', shutDown('SIGTERM'));
 
 process.on('exit', (code) => {
   console.log(`${code} received!`);
